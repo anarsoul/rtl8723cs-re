09c0375084aedf0151f6fc9602f5648de7f34804
Vasily Khoruzhick <anarsoul@gmail.com>
2024-12-29 15:04:55 -0800

    Add more register defitions and print reg address even if decoded

--- parse_sdio.py
+++ parse_sdio.py
@@ -9,26 +9,26 @@ sdio_pattern = '.*sdio_(read|write)(b|w|l): ([0-9a-f]+)=([0-9a-f]+).*'
 def decode_reg(op, size, addr, data):
     reg_addr = addr & ~0x10000
     if reg_addr in registers.keys():
-        print(f"sdio_{op}{size} {registers[reg_addr]} = {data:x}")
+        print(f"sdio_{op}{size}\t{registers[reg_addr]} = {data:x}\t\t# {reg_addr:04x}")
         return
 
     if reg_addr >= 0x1000 and reg_addr < 0x2000:
-        print(f"sdio_{op}{size} FWDL_{reg_addr - 0x1000:04x} = {data:08x}")
+        print(f"sdio_{op}{size}\tFWDL_{reg_addr - 0x1000:04x} = {data:08x}")
         return
 
     for key in registers.keys():
         if (key & 3) == 0 and reg_addr > key and (reg_addr - key) <= 3:
-            print(f"sdio_{op}{size} {registers[key]}+{reg_addr-key} = {data:x}")
+            print(f"sdio_{op}{size}\t{registers[key]}+{reg_addr-key} = {data:x}\t\t# {reg_addr:04x}")
             return
 
-    print(f"sdio_{op}{size} {addr:08x} = {data:x}")
+    print(f"sdio_{op}{size}\t{addr:08x} = {data:x}")
 
 
 def decode(op, size, addr, data):
     if addr & 0x10000:
         decode_reg(op, size, addr, data)
     else:
-        print(f"sdio_{op}{size} {addr:08x} = {data:x}")
+        print(f"sdio_{op}{size}\t{addr:08x} = {data:x}")
 
 if len(sys.argv) < 2:
     print(f"Usage: {sys.argv[0]} filaname")
